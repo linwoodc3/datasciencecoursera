7f2abb0e6dbd4261c9646945c1bc073c5f5471c7
linwoodc3 <valinvescap@gmail.com>
2016-03-05 12:00:13 -0500

    remoing all the data

--- cleaningdata/data/XMLParse.R
+++
@@ -1,8 +0,0 @@
-if (!file.exists("data")){
-    dir.create('data')
-}
-
-fileUrl <- "http://www.w3schools.com/xml/simple.xml"
-doc <- xmlTreeParse(fileUrl,useInternalNodes = TRUE)
-rootNode <- xmlRoot(doc)
-xmlName(rootNode)
