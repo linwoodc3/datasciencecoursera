a64944250359900acec5e4c829875c94aa6ef36f
linwoodc3 <valinvescap@gmail.com>
2016-03-06 03:17:28 -0500

    Finished question 1

--- exploratorydataanalysis/courseproject2/plot1.R
+++ exploratorydataanalysis/courseproject2/plot1.R
@@ -47,6 +47,10 @@ if (!file.exists('./data/files/summarySCC_PM25.rds')) {
     print(paste0("You have the target zip file. Proceeding.... "))        
 }
 
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from 
+# all sources for each of the years 1999, 2002, 2005, and 2008.
+
 ###############################################################################
 # Load the data
 ###############################################################################
